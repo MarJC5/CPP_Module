04d2647ef0ee0617e442f9966c546619aba14110
MarJC5 <jmartin@student.42lausanne.ch>
2022-10-28 18:15:06 +0200

    Ex02: HI THIS IS BRAIN

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -10,3 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main(void) {
+	std::string  str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "The address of the str in memory: " << &str << std::endl;
+	std::cout << "The address saved in stringPTR: " << stringPTR << std::endl;
+	std::cout << "The address saved in stringREF: " << &stringREF << std::endl;
+	std::cout << "--------------------------------" << std::endl;
+	std::cout << "The value of str: " << str << std::endl;
+	std::cout << "The value pointed by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed by stringREF: " << stringREF << std::endl;
+
+	return (0);
+}
