e5be7b3d8418f00a73f48494a0d0fee045c67158
MarJC5 <admin@marjc.ch>
2022-11-25 08:22:12 +0100

    CPP_07/ex02 begin

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -10,3 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template <typename T>
+class Array
+{
+	public:
+		Array<T>(void);
+		Array<T>(unsigned int n);
+		Array<T>(Array<T> const & src);
+		~Array<T>(void);
+		Array<T> &	operator=(Array<T> const & rhs);
+		T &	operator[](unsigned int i);
+		unsigned int	size(void) const;
+
+	private:
+		T *	_array;
+		unsigned int	_size;
+};
+
+template <typename T>
+Array<T>::Array(void) : _array(new T[0]), _size(0)
+{
+	return ;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n) : _array(new T[n]), _size(n)
+{
+	return ;
+}
+
+template <typename T>
+
+
+#endif
