7440d3f23d22120ed80101904d0aaa721727e54e
MarJC5 <admin@marjc.ch>
2022-11-25 08:17:35 +0100

    CPP_07/ex01

--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -10,3 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+#include <string>
+
+void	print(int const &i)
+{
+	std::cout << i << std::endl;
+}
+
+void toUpper(std::string const &str)
+{
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			std::cout << (char)(str[i] - 32);
+		else
+			std::cout << str[i];
+	}
+}
+
+int	main(void)
+{
+	{
+		int		array[5] = {1, 2, 3, 4, 5};
+
+		::iter<int>(array, 5, print);
+	}
+	{
+		std::string		array[5] = {"hello ", "world ", "this ", "is ", "a test"};
+		::iter<std::string>(array, 5, toUpper);
+	}
+	return (0);
+}
