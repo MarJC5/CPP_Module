ec0b99e6d452244cff30582d08bf5aedc1f82770
MarJC5 <jmartin@student.42lausanne.ch>
2022-11-26 15:51:38 +0100

    :^)

--- CPP_06/ex00/inc/ScalarTypes.hpp
+++ CPP_06/ex00/inc/ScalarTypes.hpp
@@ -6,7 +6,7 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 15:22:42 by jmartin           #+#    #+#             */
-/*   Updated: 2022/11/25 19:36:40 by jmartin          ###   ########.fr       */
+/*   Updated: 2022/11/26 15:33:16 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,75 +14,34 @@
 #define SCALARTYPES_HPP
 
 #include <iostream>
-#include <exception>
+#include <iomanip>
+#include <cstring>
+#include <cmath>
+#include <limits>
 #include <string>
-
-#define INT				0
-#define FLOAT			1
-#define DOUBLE			2
-#define CHAR			3
-#define PSEUDO_LITERAL	4
+#include <cctype>
 
 class ScalarTypes
 {
-	private:
-	char		*_str;
-	int			_type;
-	char		_char;
-	int			_int;
-	float		_float;
-	double		_double;
-
-	void		_detectType(void);
-
-	static bool	_isDisplayableNANChar(char *str);
-	static bool	_isInt(char *str);
-	static bool	_isFloat(char *str);
-	static bool	_isDouble(char *str);
-	static bool	_isPseudoLiteral(char *str);
-	static bool	_isFloatPseudoLiteral(std::string arg);
-	static bool	_isDoublePseudoLiteral(std::string arg);
-
-	void		_convertFromChar(void);
-	void		_convertFromNums(void);
-	void		_convertFromInt(void);
-	void		_convertFromFloat(void);
-	void		_convertFromDouble(void);
-
-	static void	_defineFormatAndPrecision(void);
-	void		_displayPseudoLiteral(void);
-	static char	*_pseudoLiteralToDouble(char *str);
-	void		_charDisplay(void);
-	void		_numberDisplay(void);
-	bool		_typeOverflow(double checkValue);
-	bool		_intOverflow(double checkValue);
-	bool		_floatOverflow(double checkValue);
-	bool		_doubleOverflow(double checkValue);
-	static void	_displayImpossible(void);
-	void		_displayChar(double checkValue);
-	static bool	_isDisplayableChar(char c);
-	void		_displayInt(double checkValue);
-	void		_displayFloat(double checkValue);
-	void		_displayDouble(double checkValue);
-
-	ScalarTypes(void);
-
-	class TypeNotFound : public std::exception
-	{
-		public:
-		virtual const char* what() const throw()
-		{
-			return ("\e[0;31mvalue is neither int, float, double or printable char\e[0m");
-		}
-	};
-
 	public:
-	ScalarTypes(char *str);
-	ScalarTypes(ScalarTypes const &instance);
-	~ScalarTypes(void);
-	ScalarTypes	&operator=(ScalarTypes const &right_hand_side);
+		ScalarTypes(void);
+		ScalarTypes(std::string str);
+		ScalarTypes(ScalarTypes const &instance);
+		~ScalarTypes(void);
+		
+		ScalarTypes	&operator=(ScalarTypes const &rhs);
 
-	void				display(void);
+		void	convert(void);
+
+	private:
+		std::string	_str;
+		float		_float;
+		double		_double;
+
+		void		_convChar(void);
+		void		_convInt(void);
+		void		_convFloat(void);
+		void		_convDouble(void);
 };
 
 #endif
--- CPP_06/ex00/srcs/main.cpp
+++ CPP_06/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 15:22:42 by jmartin           #+#    #+#             */
-/*   Updated: 2022/11/25 11:17:28 by jmartin          ###   ########.fr       */
+/*   Updated: 2022/11/26 15:33:39 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,10 @@ int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cout << "Usage: ./convert [args]" << std::endl;
+		std::cerr << "Usage: ./convert [args]" << std::endl;
 		return (1);
 	}
-	try {
-		std::string str(argv[1]);
-		ScalarTypes	value(argv[1]);
-		value.display();
-	}
-	catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
+	ScalarTypes	value(argv[1]);
+	value.convert();
 	return (0);
 }
