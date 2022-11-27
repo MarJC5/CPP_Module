1a3ae05ed60d445f9b49eeeda0520114b02df723
MarJC5 <admin@marjc.ch>
2022-11-27 04:36:34 +0100

    CPP_08/ex02 finish

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -10,3 +10,52 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+
+/*
+ * Stack is a container adapter that gives the programmer the functionality of a stack - specifically, a LIFO (last-in first-out) data structure.
+ *
+ * Elements are pushed/popped from the "back" of the specific container, which is known as the top of the stack.
+ * The functions push and pop are used to add and remove elements from the stack container, respectively.
+ * The function top returns a reference to the top element in the stack.
+ * The underlying container may be any of the standard container class templates or some other specifically designed container class.
+ * The container shall support the following operations:
+ * empty, size, back, push_back, pop_back.
+ * The standard container classes vector, deque and list fulfill these requirements.
+ * By default, if no container class is specified for a particular stack class instantiation, the standard container deque is used.
+ *
+ */
+
+template <typename T>
+class MutantStack : public std::stack<T> // inherit from std::stack
+{
+	public:
+		MutantStack() : std::stack<T>() {}
+		MutantStack(MutantStack const & src) : std::stack<T>(src) {}
+		virtual ~MutantStack() {}
+
+		MutantStack &operator=(MutantStack const &rhs)
+		{
+			if (this != &rhs)
+				std::stack<T>::operator=(rhs); // or this->c = rhs.c;
+			return (*this);
+		}
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		iterator	begin(void)
+		{
+			return (std::stack<T>::c.begin()); // or this->c.begin();
+		}
+
+		iterator	end(void)
+		{
+			return (std::stack<T>::c.end()); // or this->c.end();
+		}
+};
+
+#endif
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -10,3 +10,92 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+
+# define GREEN    "\033[1;32m"
+# define RED      "\033[1;31m"
+# define ORANGE   "\033[1;33m"
+# define CYAN     "\033[1;36m"
+# define GREY     "\033[1;37m"
+# define NC       "\033[0m"
+
+int main(void) {
+	{
+		std::cout << ORANGE << "---------------------------------" << std::endl;
+		std::cout << "Stack without iterator" << std::endl;
+		std::cout << "---------------------------------" << NC << std::endl;
+		MutantStack<float> mstack;
+
+		mstack.push(64.0f);
+		mstack.push(5.0f);
+		mstack.push(17.0f);
+		mstack.push(42.0f);
+		mstack.push(25.0f);
+
+		for (int i = 0; i < 5; i++) {
+			std::cout << mstack.top() << std::endl; // print the top of the stack
+			mstack.pop(); // remove the top element of the stack
+		}
+	}
+
+	std::cout << std::endl;
+
+	{
+		std::cout << CYAN << "---------------------------------" << std::endl;
+		std::cout << "Stack with iterator" << std::endl;
+		std::cout << "---------------------------------" << NC << std::endl;
+
+		MutantStack<std::string> mstack;
+
+		mstack.push("Hello");
+		mstack.push("World");
+		mstack.push("!");
+
+		MutantStack<std::string>::iterator it = mstack.begin(); // create an iterator to the beginning of the stack
+		MutantStack<std::string>::iterator ite = mstack.end(); // end() is the last element of the stack
+
+		for (; it != ite; it++) {
+			std::cout << *it;
+			if (it != ite)
+				std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	{
+		std::cout << GREEN << "---------------------------------" << std::endl;
+		std::cout << "Main from subject" << std::endl;
+		std::cout << "---------------------------------" << NC << std::endl;
+
+		MutantStack<int> mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+
+		std::cout << "top: " << mstack.top() << std::endl;
+
+		mstack.pop(); // remove the top element of the stack
+
+		std::cout << "size: " << mstack.size() << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+
+		mstack.push(0);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+		while (it != ite) {
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	return (0);
+}
