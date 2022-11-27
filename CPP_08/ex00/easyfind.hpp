a4c4ab8e25946639a54b62ac7c1eb53a401f2751
MarJC5 <admin@marjc.ch>
2022-11-27 01:30:03 +0100

    CPP_08/ex00

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -10,3 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <algorithm>
+# include <vector>
+
+/*
+ * A vector is a sequence container that encapsulates dynamic size arrays.
+ * Just like arrays, vectors use contiguous storage locations for their elements,
+ * which means that their elements can also be accessed using offsets on regular
+ * pointers to its elements, and just as efficiently as in arrays.
+ *
+ * But unlike arrays, their size can change dynamically, with their storage
+ * being handled automatically by the container.
+ *
+ * Internally, vectors use a dynamically allocated array to store their elements.
+ *
+ * This array may need to be reallocated in order to grow in size when new
+ * elements are inserted, which implies allocating a new array and moving all
+ * elements to it. This is a relatively expensive task in terms of processing
+ * time, and thus, vectors do not reallocate each time an element is added to
+ * the container.
+ */
+
+class NotFoundException : public std::exception
+{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("Value not found");
+		}
+};
+
+template <typename T>
+typename T::iterator	easyfind(T &container, int value)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw NotFoundException();
+	return (it);
+}
+
+#endif
--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -10,3 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
+int main(void) {
+	std::vector<int> v; // vector of ints
+	std::vector<int>::iterator it; // iterator to a vector of int
+
+	for (int i = 0; i < 10; i++)
+		v.push_back(i); // fill the vector with 0 to 9 values (dynamic allocation)
+
+	// Will fail and throw an exception because 42 is not in the vector
+	try {
+		it = easyfind(v, 42);
+		std::cout << "Value found: " << *it << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	// Will succeed and print the value 2
+	try {
+		it = easyfind(v, 2);
+		std::cout << "Value found: " << *it << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	return (0);
+}
